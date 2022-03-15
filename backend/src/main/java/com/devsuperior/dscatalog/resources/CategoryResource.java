59ba4a754d2c3b05309bf304b270086bae8a1ec8
renatomak <renato.mark.silva@gmail.com>
2022-03-15 19:16:04 -0300

    get by id

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -6,6 +6,7 @@ import com.devsuperior.dscatalog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class CategoryResource {
     @GetMapping
     public ResponseEntity<List<CategoryDto>> findAll() {
         return ResponseEntity.ok(categoryService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDto> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.findById(id));
     }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/service/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/service/CategoryService.java
@@ -5,8 +5,10 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositoies.CategoryRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,5 +20,10 @@ public class CategoryService {
     public List<CategoryDto> findAll() {
         List<Category> categories = categoryRespository.findAll();
         return categories.stream().map(CategoryDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDto findById(Long id) {
+        return new CategoryDto(categoryRespository.findById(id).get());
     }
 }
