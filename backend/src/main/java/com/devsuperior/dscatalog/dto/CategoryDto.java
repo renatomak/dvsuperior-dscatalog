18aa176f32523343aca0a3917f9b1a4062bf5813
renatomak <renato.mark.silva@gmail.com>
2022-03-19 00:13:12 -0300

    ProductDto

--- backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDto.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDto.java
@@ -3,13 +3,16 @@ package com.devsuperior.dscatalog.dto;
 import com.devsuperior.dscatalog.entities.Category;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-public class CategoryDto {
+public class CategoryDto implements Serializable {
+    private static final long serialVersionUID = 7667553656723820335L;
 
     @EqualsAndHashCode.Include
     private Long id;
---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDto.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductDto implements Serializable {
+    private static final long serialVersionUID = 7193684038002132400L;
+
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+    
+    private List<CategoryDto> categoryDtos = new ArrayList<>();
+
+    private ProductDto(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.description = entity.getDescription();
+        this.price = entity.getPrice();
+        this.imgUrl = entity.getImgUrl();
+    }
+
+    private ProductDto(Product entity, Set<Category> categories) {
+        this(entity);
+        categories.forEach(cat -> this.categoryDtos.add(new CategoryDto(cat)));
+    }
+}
